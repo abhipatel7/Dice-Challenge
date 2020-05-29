fee09ec181b3b93bb158a4b328415f3e9dd1ed1a
abhipatel7 <52331303+abhipatel7@users.noreply.github.com>
2020-05-29 19:01:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var imagesArray = ["images/dice 1.png", "images/dice 2.png", "images/dice 3.png", 
+    "images/dice 4.png", "images/dice 5.png", "images/dice 6.png"];
+
+var randomNumber1 = Math.floor(Math.random() * imagesArray.length);
+document.querySelector(".img1").setAttribute("src", imagesArray[randomNumber1]);
+
+var randomNumber2 = Math.floor(Math.random() * imagesArray.length);
+document.querySelector(".img2").setAttribute("src", imagesArray[randomNumber2]);
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "<img class='victory' src='images/victory.png'>Player1 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Player2 Wins!<img class='victory' src='images/victory.png'>";
+}
