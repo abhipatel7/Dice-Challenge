e04e4693122ca2f81b4d290bbb0c21d14b4ec17b
abhipatel7 <52331303+abhipatel7@users.noreply.github.com>
2020-05-29 19:14:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ document.querySelector(".img2").setAttribute("src", imagesArray[randomNumber2]);
 
 if(randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "<img class='victory' src='images/victory.png'>Player1 Wins!";
-} else {
+} if(randomNumber2 > randomNumber1) {
     document.querySelector("h1").innerHTML = "Player2 Wins!<img class='victory' src='images/victory.png'>";
+} else {
+    document.querySelector("h1").innerHTML = "It's a Draw!";
 }
